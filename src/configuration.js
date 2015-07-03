3f8a888b8deae8eee8ff05ecb4a38ebafea3b40e
Ian Donnelly <easyxtarget@gmail.com>
2015-07-03 18:58:13 -0400

    First Release
    First release!

--- src/configuration.js
+++ src/configuration.js
@@ -6,19 +6,20 @@ Pebble.addEventListener('ready', function(e) {
 Pebble.addEventListener('showConfiguration', function(e) {
   // Show config page
   console.log('Showing config page');
-  Pebble.openURL('https://dl.dropboxusercontent.com/u/31585964/SportOfficialConfig.html');
+  Pebble.openURL('https://dl.dropboxusercontent.com/u/31585964/SportsOfficialConfig.html');
 });
 
 Pebble.addEventListener("webviewclosed", function(e) {
     //Get JSON dictionary
     console.log("Webview closed");
+    console.log(e.response);
     var config = JSON.parse(decodeURIComponent(e.response));
     console.log("Config: " + JSON.stringify(config));
-  
+    
     var single = parseInt(config.single);
     var long = parseInt(config.long);
     var double = parseInt(config.double);
-
+  
     //Send to Pebble
     Pebble.sendAppMessage(
       {"SINGLE_CLICK_KEY": single, "LONG_CLICK_KEY": long, "DOUBLE_CLICK_KEY": double},
