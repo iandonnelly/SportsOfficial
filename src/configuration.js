bfcc24b67fa300eccb98d00700973abd49e56e06
Ian Donnelly <easyxtarget@gmail.com>
2015-08-03 22:15:40 -0400

    Update App for New Configuration Page
    App now supports v1.1 of the configuration page which is way better looking.

--- src/configuration.js
+++ src/configuration.js
@@ -6,29 +6,31 @@ Pebble.addEventListener('ready', function(e) {
 Pebble.addEventListener('showConfiguration', function(e) {
   // Show config page
   console.log('Showing config page');
-  Pebble.openURL('https://dl.dropboxusercontent.com/u/31585964/SportsOfficialConfig.html');
+  Pebble.openURL('https://dl.dropboxusercontent.com/u/31585964/SportsOfficialConfig_1.1.html');
 });
 
 Pebble.addEventListener("webviewclosed", function(e) {
     //Get JSON dictionary
     console.log("Webview closed");
     console.log(e.response);
-    var config = JSON.parse(decodeURIComponent(e.response));
-    console.log("Config: " + JSON.stringify(config));
+    if(e.response != "cancel"){
+      var config = JSON.parse(decodeURIComponent(e.response));
+      console.log("Config: " + JSON.stringify(config));
+      
+      var single = parseInt(config.single);
+      var long = parseInt(config.long);
+      var double = parseInt(config.double);
     
-    var single = parseInt(config.single);
-    var long = parseInt(config.long);
-    var double = parseInt(config.double);
-  
-    //Send to Pebble
-    Pebble.sendAppMessage(
-      {"SINGLE_CLICK_KEY": single, "LONG_CLICK_KEY": long, "DOUBLE_CLICK_KEY": double},
-      function(e) {
-        console.log("Sending settings data...");
-      },
-      function(e) {
-        console.log("Settings feedback failed!");
-      }
-    );
+      //Send to Pebble
+      Pebble.sendAppMessage(
+        {"SINGLE_CLICK_KEY": single, "LONG_CLICK_KEY": long, "DOUBLE_CLICK_KEY": double},
+        function(e) {
+          console.log("Sending settings data...");
+        },
+        function(e) {
+          console.log("Settings feedback failed!");
+        }
+      );     
+    }
   }
 );
